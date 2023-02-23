dfae88def7002ffd2668cc6531697be3edc0e9c7
Shivam Kamboj <shivam1kamboj@gmail.com>
2023-02-22 20:19:58 -0800

    new notebook added in master branch

--- M_matrix/M_matrix.py
+++ M_matrix/M_matrix.py
@@ -3,10 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Defining matrix M
 
 def qim_matrix(g, N, J):
-
     M = np.zeros((2 * N, 2 * N + 1))
 
     for i, c in enumerate(M[:N]):
@@ -15,14 +15,8 @@ def qim_matrix(g, N, J):
         c[i - 1] = -1 if i > 0 else 0
         c[i + 1] = -1 if i < N - 1 else 0
 
-        c[N + i - 1] = 1 if i > 0 else 0
-        c[N + i + 1] = 1 if i < N else 0
-
     for i, c in enumerate(M[N:]):
         c[N + i] = -2 * g
-
-        c[i - 1] = 1 if i > 0 else 0
-        c[i + 1] = 1 if i < N - 1 else 0
 
         c[N + i - 1] = 1 if i > 0 else 0
         c[N + i + 1] = 1 if i < N - 1 else 0
@@ -41,10 +35,8 @@ ax.set_aspect('equal')
 plt.colorbar()
 plt.savefig('/Users/shivamkamboj/Documents/UC_Merced/Research/Spring_2023/QIM/M_matrix/M_for_N_4.pdf')
 
-
 plt.pcolormesh(qim_matrix(g=0.3, N=10, J=1), edgecolors='k', linewidth=1)
 plt.title(f"M-matrix for N =10", fontsize=20)
 ax = plt.gca()
 ax.set_aspect('equal')
 plt.savefig('/Users/shivamkamboj/Documents/UC_Merced/Research/Spring_2023/QIM/M_matrix/M_for_N_10.pdf')
-
